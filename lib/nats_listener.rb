fb070870aa00fc0a2936bc6e2fea0c1245d2816f
Igor Pavlov <gophan1992@gmail.com>
2018-10-15 13:46:31 +0300

    added streaming subscriber

--- lib/nats_listener.rb
+++ lib/nats_listener.rb
@@ -2,7 +2,9 @@ require 'nats_listener/version'
 require 'nats_listener/client'
 require 'nats_listener/streaming_client'
 require 'nats_listener/subscriber'
+require 'nats_listener/streaming_subscriber'
 require 'nats_listener/nats_message.pb'
 require 'nats_listener/message'
+
 module NatsListener
 end
