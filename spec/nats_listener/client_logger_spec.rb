522da27602a6e23d6e5047d0f2c32d72a97e1215
Igor Pavlov <gophan1992@gmail.com>
2018-10-17 17:52:36 +0300

    added more tests for logger

--- spec/nats_listener/client_logger_spec.rb
+++ spec/nats_listener/client_logger_spec.rb
@@ -14,4 +14,25 @@ RSpec.describe NatsListener::ClientLogger do
       end
     end
   end
+
+  describe '#log' do
+    let(:obj) { described_class.new(opts) }
+    subject { obj.log(action: 'action', message: 'msg', service_name: 'service') }
+
+    context 'with logger' do
+      it 'should call info' do
+        expect(logger).to receive(:info)
+        subject
+      end
+    end
+
+    context 'without logger' do
+      let(:logger) { nil }
+
+      it 'should not call info' do
+        expect(logger).not_to receive(:info)
+        subject
+      end
+    end
+  end
 end
